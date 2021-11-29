b9b818ffe3a36166bd4aaa310867e065d8b6fb2f
Matty Wong <wonghzm@stanford.edu>
2021-11-28 22:16:56 -0800

    Preliminary PR

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <cmath>
+#define OMP_NUM_THREADS		32
 #include <omp.h>
 #include <utility>
 
@@ -23,11 +24,66 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
   // initialize vertex weights to uniform probability. Double
   // precision scores are used to avoid underflow for large graphs
 
-  int numNodes = num_nodes(g);
-  double equal_prob = 1.0 / numNodes;
-  for (int i = 0; i < numNodes; ++i) {
-    solution[i] = equal_prob;
-  }
+    int numNodes = num_nodes(g);
+    double equal_prob = 1.0 / numNodes;
+	double damp_coeff = damping / numNodes;
+	double tele_coeff = equal_prob - damp_coeff;
+    int* sinks((int*)calloc(numNodes, sizeof(int)));
+    int numSinks = 0;
+  
+    double* new_scores((double*)calloc(numNodes, sizeof(double)));
+    if (new_scores == nullptr || sinks == nullptr) {
+		printf("\nERROR: Unable to malloc temporary arrays\n");
+		abort();
+    }
+  
+    #pragma omp parallel for
+    for (int i = 0; i < numNodes; ++i) {
+		solution[i] = equal_prob;
+		if (outgoing_size(g, i) == 0) {
+			#pragma omp capture
+			int idx = numSinks++;
+			sinks[idx] = i;
+		}
+    }
+  
+	double curr = 0;
+	double prev = 0xDEADBEEF;
+	
+	while (abs(curr - prev) > convergence) {
+		prev = curr;
+		curr = 0;
+		double curr_tele_add = 0;
+		
+		#pragma omp parallel for reduction(+:curr_tele_add)
+		for (int i = 0; i < numSinks; ++i) {
+			curr_tele_add += solution[sinks[i]];
+		}
+		curr_tele_add *= damp_coeff;
+		
+		#pragma omp parallel for schedule(guided) reduction(+:curr)
+		for (int i = 0; i < numNodes; ++i) {
+			// Vertex* points into g.outgoing_edges[]
+			double new_score = 0;
+			const Vertex* start = incoming_begin(g, i);
+			const Vertex* end = incoming_end(g, i);
+			for (const Vertex* v = start; v != end; v++) {
+				new_score += solution[*v] / outgoing_size(g, *v);
+			}
+			new_score = new_score * damping + tele_coeff + curr_tele_add;
+			curr += abs(new_score - solution[i]);
+			new_scores[i] = new_score;
+		}
+		
+		#pragma omp parallel for 
+		for (int i = 0; i < numNodes; ++i) {
+			solution[i] = new_scores[i];
+		}
+	}
+    
+	free(sinks);
+	free(new_scores);
+}  
   
   
   /*
@@ -58,4 +114,4 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
      }
 
    */
-}
+
