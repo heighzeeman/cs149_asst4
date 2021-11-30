f6fa8f0c316be50eb5e1f8c84027282b60f44335
Matty Wong <wonghzm@stanford.edu>
2021-11-29 16:24:19 -0800

    Revert to max nodes

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -86,7 +86,7 @@ void bfs_top_down(Graph graph, solution* sol) {
     sol->distances[ROOT_NODE_ID] = 0;
 	vertex_set scratch[omp_get_max_threads()];
 	for (int i = 0; i < omp_get_max_threads(); ++i) {
-		scratch[i].vertices = new int[graph->num_edges];
+		scratch[i].vertices = new int[graph->num_nodes];
 		scratch[i].count = 0;
 	}
 
